540841485fa7bacc19c25007d2afa526bbb8eabe
BereketW <bereketsodenoo@gmail.com>
2025-02-19 14:10:24 +0300

    project setup finished

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import Services from "@/components/services";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Services />
+    </div>
+  );
+}
